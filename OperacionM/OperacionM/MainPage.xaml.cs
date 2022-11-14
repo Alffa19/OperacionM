8e676dda0c37004d4195c9d20b1b1c9771fae71a
alfre <alfre@DESKTOP-S1GAVLV>
2022-11-14 10:58:41 -0600

    Agregar archivos de proyecto.

---
+++ OperacionM/OperacionM/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OperacionM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsuma_Clicked(object sender, EventArgs e)
+        {
+            var ope = new Models.Operaciones
+            {
+                Numero1 = Convert.ToDouble(txtnumero1.Text),
+                Numero2 = Convert.ToDouble(txtnumero2.Text),
+                Resultado = Convert.ToDouble(txtnumero1.Text) + Convert.ToDouble(txtnumero2.Text)
+            };
+            var page = new Views.PageTwo();
+            page.BindingContext = ope;
+            await Navigation.PushModalAsync(page);
+        }
+
+        private async void btnrestar_Clicked(object sender, EventArgs e)
+        {
+            var ope = new Models.Operaciones
+            {
+                Numero1 = Convert.ToDouble(txtnumero1.Text),
+                Numero2 = Convert.ToDouble(txtnumero2.Text),
+                Resultado = Convert.ToDouble(txtnumero1.Text) - Convert.ToDouble(txtnumero2.Text)
+            };
+            var page = new Views.PageTwo();
+            page.BindingContext = ope;
+            await Navigation.PushModalAsync(page);
+        }
+
+        private async void btndividir_Clicked(object sender, EventArgs e)
+        {
+            var ope = new Models.Operaciones
+            {
+                Numero1 = Convert.ToDouble(txtnumero1.Text),
+                Numero2 = Convert.ToDouble(txtnumero2.Text),
+                Resultado = Convert.ToDouble(txtnumero1.Text) / Convert.ToDouble(txtnumero2.Text)
+            };
+            var page = new Views.PageTwo();
+            page.BindingContext = ope;
+            await Navigation.PushModalAsync(page);
+        }
+
+        private async void btnmultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var ope = new Models.Operaciones
+            {
+                Numero1 = Convert.ToDouble(txtnumero1.Text),
+                Numero2 = Convert.ToDouble(txtnumero2.Text),
+                Resultado = Convert.ToDouble(txtnumero1.Text) * Convert.ToDouble(txtnumero2.Text)
+            };
+            var page = new Views.PageTwo();
+            page.BindingContext = ope;
+            await Navigation.PushModalAsync(page);
+        }
+    }
+}
---
+++ OperacionM/OperacionM/Models/Operaciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperacionM.Models
+{
+    class Operaciones
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
